84daaaaf47dc3ddb9dd782a63217500d6e30148e
Christian Fiedler <christian.fiedler@bhrtech.com>
2019-06-06 18:15:40 +0200

    Replace mdate() with vlc_tick_now() and add udp to fix

--- src/input/input.c
+++ src/input/input.c
@@ -693,7 +693,9 @@ static void MainLoop( input_thread_t *p_input, bool b_interactive )
                 if( b_can_demux )
                     i_wakeup = es_out_GetWakeup( input_priv(p_input)->p_es_out );
                 else if(p_demux->psz_url != NULL && !strncmp(p_demux->psz_url, "rtp:", 4))
-                    i_wakeup = mdate() + INPUT_IDLE_SLEEP;
+                    i_wakeup = vlc_tick_now() + INPUT_IDLE_SLEEP;
+                else if(p_demux->psz_url != NULL && !strncmp(p_demux->psz_url, "udp:", 4))
+                    i_wakeup = vlc_tick_now() + INPUT_IDLE_SLEEP;
                 if( b_force_update )
                     i_intf_update = 0;
 
